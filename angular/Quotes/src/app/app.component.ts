c06d8eb04a0c13d2c954cff3088bd4ee7c26fefd
tonyishangu <tonyishangu@gmail.com>
2020-08-22 11:30:06 +0300

    add a detail and delete button

--- angular/Quotes/src/app/app.component.ts
+++ angular/Quotes/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Quote } from './quote';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes:Quote[]=[
+    { id: 1, name: 'J Cole', description: 'Love Yours' },
+    { id: 2, name: 'Bob Proctor', description: 'As a man thinketh so is he' },
+    { id: 2, name: 'Kevin Kruse', description: 'Life is about making impact' }
+  ];
 }
