9d8e7acbf0e7ddf33214281670ed5c45cdb43c95
tonyishangu <tonyishangu@gmail.com>
2020-08-23 14:03:05 +0300

    add bootstrap

--- angular/Quotes/src/app/quote/quote.component.ts
+++ angular/Quotes/src/app/quote/quote.component.ts
@@ -18,6 +18,15 @@ export class QuoteComponent implements OnInit {
   toggleDetails(index) {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+
+      if (toDelete) {
+        this.quotes.splice(index, 1)
+      }
+    }
+  }
 
   constructor() { }
 
